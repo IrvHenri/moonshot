777ec80a67f905f7cc45322f6dc37c76cd802891
Zobee <zohebboga@hotmail.com>
2021-07-04 17:48:36 -0400

    Implemented ui for login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,44 @@
+import "./Login.css"
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { Button } from "@material-ui/core";
 const Login = () => {
-  return <div>
-    Log In
+  return <div className='login-section'>
+    <img className="login-img" src="img/stock_login.jpg" />
+    <div className='login-form'>
+    <h1>Log In</h1>
+    <form>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Grid container spacing={4}>
+              <Grid item xs={12}>
+                <TextField 
+                fullWidth
+                label="Email" 
+                name="email" 
+                size="small" 
+                variant="outlined" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  size="small"
+                  type="password"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+          <Button fullWidth color="secondary" type="submit" variant="contained">
+              Log in
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
   </div>
 }
 
