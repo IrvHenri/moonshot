eb51a1a68147ab95bb85b751ba5fa90fd0a4d68d
Zobee <zohebboga@hotmail.com>
2021-07-03 21:11:47 -0400

    Made navbar mobile-responsive and implemented a very basic toggleable drop down

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,19 @@
 import "./Navbar.css"
-import {Link} from 'react-router-dom'
 import { useState } from 'react'
+import NavLinks from "./NavLinks"
 const Navbar = () => {
-  const [loggedIn, setLoggedIn] = useState(true) //We'll change this later, just a temp way to toggle for now
-  const [active, setActive] = useState(null);
-  const signOut = () => {
-    setLoggedIn(false)
-  }
+  const [toggle, setToggle] = useState(false)
   return <nav className='navbar'>
     <div className='nav-left'>
     <h1 className='nav-logo'>Moonshot</h1>
-    <div className='nav-links'>
-      <Link to="/">Home</Link>
-      <Link to="/prices">Prices</Link>
-      <Link to="/portfolio">My Portfolio</Link>
-      {loggedIn ? <Link to='/' onClick={signOut}>Sign Out</Link> : <Link to="/login">Log In</Link>}
-    </div>
+      <NavLinks toggle={toggle} setToggle={setToggle} />
     </div>
     <div className='nav-right'>
-      <input type='text'/>
+      <input type='text' placeholder="bitcoin"/>
       <button>Search</button>
     </div>
-    <div className='burger'>
-      <span>---</span>
-      <span>---</span>
-      <span>---</span>
+    <div onClick={() => setToggle(prev => !prev)} className='burger'>
+      CLICK
     </div>
   </nav>
 }
