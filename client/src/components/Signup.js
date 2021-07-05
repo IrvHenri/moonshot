3f97558406371a24a34530a2e53b35e47e109e24
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-07-05 14:24:42 -0400

    fix whitespace and indentation

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -8,69 +8,71 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPw, setConfirmPw] = useState("")
-  return <div className='login-section'>
-    <div className='login-form'>
-    <h1>Sign Up</h1>
-    <form>
-      <div>
-      <TextField 
-       fullWidth
-       label="Name" 
-       name="name" 
-       size="small" 
-       variant="outlined"
-       value={name}
-       onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-      <TextField 
-       fullWidth
-       label="Email" 
-       name="email" 
-       size="small" 
-       variant="outlined"
-       value={email}
-       onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-      <TextField
+  return (
+    <div className='login-section'>
+      <div className='login-form'>
+      <h1>Sign Up</h1>
+      <form>
+        <div>
+        <TextField 
         fullWidth
-        label="Password"
-        name="password"
-        size="small"
-        type="password"
+        label="Name" 
+        name="name" 
+        size="small" 
         variant="outlined"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <div>
-      <TextField
-        fullWidth
-        label="Confirm Password"
-        name="confirmPw"
-        size="small"
-        type="password"
-        variant="outlined"
-        value={confirmPw}
-        onChange={(e) => setConfirmPw(e.target.value)} />
-      </div>
-      <div>
-      <Button fullWidth color="primary" type="submit" variant="contained">
-        Sign Up
-      </Button>
-      </div>
-
-      </form>
-      <Link className='signup-link' to={"/login"}>
-        <div className='signup-btn'>
-          <Button fullWidth color="primary">
-            Already A User? Log In!
-          </Button>
+        value={name}
+        onChange={(e) => setName(e.target.value)} />
         </div>
-      </Link>
+        <div>
+        <TextField 
+        fullWidth
+        label="Email" 
+        name="email" 
+        size="small" 
+        variant="outlined"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+        <TextField
+          fullWidth
+          label="Password"
+          name="password"
+          size="small"
+          type="password"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div>
+        <TextField
+          fullWidth
+          label="Confirm Password"
+          name="confirmPw"
+          size="small"
+          type="password"
+          variant="outlined"
+          value={confirmPw}
+          onChange={(e) => setConfirmPw(e.target.value)} />
+        </div>
+        <div>
+        <Button fullWidth color="primary" type="submit" variant="contained">
+          Sign Up
+        </Button>
+        </div>
+
+        </form>
+        <Link className='signup-link' to={"/login"}>
+          <div className='signup-btn'>
+            <Button fullWidth color="primary">
+              Already A User? Log In!
+            </Button>
+          </div>
+        </Link>
+      </div>
+      <img className="login-img" src="img/stock_signup.jpg" alt=""/>
     </div>
-    <img className="login-img" src="img/stock_signup.jpg" />
-  </div>
+  )
 }
 
 export default Login;
