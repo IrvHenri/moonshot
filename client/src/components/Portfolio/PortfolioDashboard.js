fa6391e29bb7cc3ab36e6a6f4a5ca52a1b149d18
Zobee <zohebboga@hotmail.com>
2021-07-05 14:04:03 -0400

    Minor style and layout changes

--- client/src/components/Portfolio/PortfolioDashboard.js
+++ client/src/components/Portfolio/PortfolioDashboard.js
@@ -1,14 +1,28 @@
 const PortfolioDashboard = () => {
   return <div className='portfolio-dashboard'>
-    <div className='portfolio-top-banner'>
-      Top Banner
+    <div className='portfolio-banner'>
+      <div className='portfolio-banner-left'>
+      <div>
+      <h1>Welcome Back User!</h1>
+      <h2>My Portfolio</h2>
+      </div>
+      <div>
+        <h2>Balance: $34,000.00</h2>
+        <p>+3.00%</p>
+      </div>
+      </div>
+      <div className='portfolio-banner-right'>
+        <button>
+          Add Coin:
+        </button>
+      </div>
     </div>
     <div className='portfolio-info-container'>
       <div className='portfolio-graph'>
-        Graph
+        <h1>Graph:</h1>
       </div>
       <div className='portfolio-coin-data'>
-        Coin Data
+        <h1>Your Assets:</h1>
       </div>
     </div>
   </div>
