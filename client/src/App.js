3c6496ed04fc74e0327bd1a3c9de66f11650da8a
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-07-05 10:44:05 -0400

    fix path to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/index";
 import Landing from "./components/Landing";
 import CurrencyTable from "./components/Currencies/index";
 import CoinDetail from "./components/CoinDetail";
