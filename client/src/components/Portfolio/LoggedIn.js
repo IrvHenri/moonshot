cc5aabf8ac9024f8b72905751079705147ed168b
Zobee <zohebboga@hotmail.com>
2021-07-05 10:30:40 -0400

    Created basic html layout for portfolio dashboard

--- client/src/components/Portfolio/LoggedIn.js
+++ client/src/components/Portfolio/LoggedIn.js
@@ -5,23 +5,21 @@ const LoggedIn = () => {
   const [userHasPortfolio, setUserHasPortfolio] = useState(false) // Gonna change this, this is just for testing
   return userHasPortfolio 
   ? 
+  //View My Portfolio
   <div>Logged In!</div> 
   :
+  //Create A New Portfolio
   <div className="portfolio-jumbo">
   <article className="portfolio-signup-article">
-    <h1>Create A Model Portfolio!</h1>
-    <h3>Moonshot Portfolio tracker</h3>
+    <h3>Create A Model Portfolio!</h3>
     <p>
-      Track Crypto Assets and stay up to date with the performance of your
-      investments. Profits, losses and portfolio valuations, we've got you
-      covered.
+      There's no better way to get started investing. Create a model portfolio to start practicing today!
     </p>
     <div className="portfolio-signup-links">
-      <button onClick={() => setUserHasPortfolio(true)}>Create your Portfolio Today</button>
+      <button className='create-portfolio-btn' onClick={() => setUserHasPortfolio(true)}>
+        Create your Portfolio
+      </button>
     </div>
-  </article>
-  <article className="portfolio-graph-article">
-    <img src={sampleGraph} alt="sample graph" />
   </article>
 </div>
 }
---
+++ client/src/components/Portfolio/PortfolioDashboard.js
@@ -0,0 +1,17 @@
+const PortfolioDashboard = () => {
+  return <div className='portfolio-dashboard'>
+    <div className='portfolio-top-banner'>
+
+    </div>
+    <div className='portfolio-info-container'>
+      <div className='portfolio-graph'>
+
+      </div>
+      <div className='portfolio-coin-data'>
+
+      </div>
+    </div>
+  </div>
+}
+
+export default PortfolioDashboard;
