334990a96b3192536816115fb5ec6752808d44e6
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-07-06 19:54:33 -0400

    basic user authentication complete

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/userModel');
 
 //Sign Up
 router.post('/signup', function(req, res, next) {
-  res.send('respond with a resource');
+
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const newUser = new User({name, email, password});
+
+  //save new user to database
+  newUser.save()
+    .then(() => res.json("User Added!"))
+    .catch(error => res.status(400).json('Error: ' + error));
+});
+
+//to test login function
+router.get('/', function(req, res, next) {
+  User.find() //find is a mongodb function to find user
+    .then(users => res.json(users))
+    .catch(error => res.status(400).json('Error: ' + error));
 });
 
 //Log In
 router.post('/login', function(req, res, next) {
-  res.send('respond with a resource');
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log('email', email);
+  console.log('password', password);
+
+  User.findOne({email})
+    .then((data) => {
+      if (data === null) {
+        res.status(404).json("email does not exist");
+      }
+
+      if (data.password === password) {
+        res.json(data);
+      } else {
+        res.status(400).json("wrong password");
+      }
+
+    })
+    .catch(error => res.status(400).json(error));
+
 });
 
 
