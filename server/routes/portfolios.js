b38b976de61ee6a2604d255eaea7acd9b13c4d83
Zobee <zohebboga@hotmail.com>
2021-07-07 19:30:05 -0400

    Remove coin DELETE route complete

--- server/routes/portfolios.js
+++ server/routes/portfolios.js
@@ -49,12 +49,34 @@ router.post("/:coinId", function(req, res) {
 /* Update a single coin in the user's portfolio */
 router.put("/:coinId", function(req, res) {
   const {quantity, purchasePrice} = req.body;
+  User.findById("60e4bbcfe0f013cb5cf16465")
+  .then(user => {
+    user.portfolio.coins.push(
+      {
+        id: req.params.coinId,
+        quantity,
+        purchasePrice
+      }
+    )
 
+    user.save()
+    .then(() => res.json("Coin Added to portfolio"))
+    .catch(err => res.status(400).json(`Error ${err}`))
+  })
+  .catch(err => res.status(400).json(`Error ${err}`))
 })
 
 /* Remove a single coin from a uswr's portfolio */
 router.delete("/:coinId", function(req, res) {
-  
+  User.findById("60e4bbcfe0f013cb5cf16465")
+  .then(user => {
+    user.portfolio.coins = user.portfolio.coins.filter(coin => coin.id !== req.params.coinId)
+
+    user.save()
+    .then(() => res.json("Coin removed from portfolio"))
+    .catch(err => res.status(400).json(`Error ${err}`))
+  })
+  .catch(err => res.status(400).json(`Error ${err}`))
 })
 
 module.exports = router;
