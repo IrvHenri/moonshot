807ede1a3c923bb8af1b07f008366275eefc4730
Irving Henriquez <henriquezirv@gmail.com>
2021-07-09 14:41:39 -0400

    added back button on coin detail page

--- client/src/components/Coin/DetailHeader.js
+++ client/src/components/Coin/DetailHeader.js
@@ -2,6 +2,8 @@ import "./CoinDetail.css";
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import currencyFormatterHelpers from "../../helpers/currencyFormatterHelpers";
+import { Link } from "react-router-dom";
+import Button from "../Button";
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
 const { currencyFormatter } = currencyFormatterHelpers();
 export default function DetailHeader({ coin }) {
@@ -18,40 +20,47 @@ export default function DetailHeader({ coin }) {
     },
   } = coin;
   return (
-    <header className=" coin-detail-header">
-      <div className="header-left">
-        <div>
-          <Avatar alt="coin logo" src={thumb} />
-          <h1>{name}</h1> <small>{symbol.toUpperCase()}</small>
-        </div>
+    <div>
+      <Link to="/currencies">
+        <Button text="back" />
+      </Link>
 
-        <p>Rank #{market_cap_rank}</p>
-      </div>
-      <div className="header-right">
-        <p>
-          {name} Price ({symbol.toUpperCase()})
-        </p>
-        <div>
-          {" "}
-          <h1>{currencyFormatter.format(current_price.usd)}</h1>{" "}
-          <small>
-            {price_change_percentage_24h > 0 ? (
-              <TiArrowSortedUp color="16C784" />
-            ) : (
-              <TiArrowSortedDown color="EA3A43" />
-            )}
-            {price_change_percentage_24h.toFixed(2)}%
-          </small>
+      <header className=" coin-detail-header">
+        <div className="header-left">
+          <div>
+            <Avatar alt="coin logo" src={thumb} />
+            <h1>{name}</h1> <small>{symbol.toUpperCase()}</small>
+          </div>
+
+          <p>Rank #{market_cap_rank}</p>
         </div>
-        <div className="high-low-price">
+        <div className="header-right">
           <p>
-            Low (24h): <small>{currencyFormatter.format(low_24h.usd)}</small>
+            {name} Price ({symbol.toUpperCase()})
           </p>
-          <p>
-            High (24h): <small>{currencyFormatter.format(high_24h.usd)}</small>
-          </p>
+          <div>
+            {" "}
+            <h1>{currencyFormatter.format(current_price.usd)}</h1>{" "}
+            <small>
+              {price_change_percentage_24h > 0 ? (
+                <TiArrowSortedUp color="16C784" />
+              ) : (
+                <TiArrowSortedDown color="EA3A43" />
+              )}
+              {price_change_percentage_24h.toFixed(2)}%
+            </small>
+          </div>
+          <div className="high-low-price">
+            <p>
+              Low (24h): <small>{currencyFormatter.format(low_24h.usd)}</small>
+            </p>
+            <p>
+              High (24h):{" "}
+              <small>{currencyFormatter.format(high_24h.usd)}</small>
+            </p>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
