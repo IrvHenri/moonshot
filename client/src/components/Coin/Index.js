2ad09e1cc048125c7904bd3233dc24073372ac9d
Irving Henriquez <henriquezirv@gmail.com>
2021-07-09 19:43:23 -0400

    adjusted coin table and coin detail styling, added icon images to switch

--- client/src/components/Coin/Index.js
+++ client/src/components/Coin/Index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 import { useParams } from "react-router-dom";
 import "./CoinDetail.css";
 import DetailHeader from "./DetailHeader";
@@ -8,20 +9,23 @@ import DetailDescription from "./DetailDescription";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import useCoinData from "../../hooks/useCoinData";
 import useSingleCoinData from "../../hooks/useSingleCoinData";
-
 export default function CoinDetail() {
   const { id } = useParams();
   const [coins] = useCoinData();
   const { coin, loading, chartData } = useSingleCoinData(id);
-
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="coin-detail-page">
+    <div
+      className={`coin-detail-page  ${
+        theme === "light" ? "coin-detail-light" : null
+      }`}
+    >
       {loading ? (
         <div className="table-loading">
           <CircularProgress size={100} />
         </div>
       ) : (
-        <>
+        <div className="coin-detail-container">
           <DetailHeader coin={coin} />
 
           <main className="coin-detail-main">
@@ -29,7 +33,7 @@ export default function CoinDetail() {
             <DetailStatCard coin={coin} chartData={chartData} coins={coins} />
           </main>
           <DetailDescription coin={coin} />
-        </>
+        </div>
       )}
     </div>
   );
