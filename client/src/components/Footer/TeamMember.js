b32eccf6c4bf7444e7ec5f5246b43716153566f6
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-07-12 01:34:06 -0400

    resize footer pictures and icons

--- client/src/components/Footer/TeamMember.js
+++ client/src/components/Footer/TeamMember.js
@@ -7,14 +7,14 @@ const TeamMember = ({ name, img, bio, linkedInUrl, gitHubUrl }) => {
       <div className="member-info">
         <h1>{name}</h1>
         <p>{bio}</p>
-      </div>
-      <div className="member-socials">
+        <div className="member-socials">
         <a href="http://www.github.com/">
           <AiFillGithub size={"3.3em"} />
         </a>
         <a href="http://www.linkedIn.com/">
           <AiFillLinkedin size={"3.3em"} />
         </a>
+      </div>
       </div>
     </div>
   );
