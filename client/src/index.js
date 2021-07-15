534dbfedb127a19dff1f9c3c20b7bbe8ec91f3fe
Zobee <zohebboga@hotmail.com>
2021-07-15 19:26:25 -0400

    Added Netlify config file

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 import App from "./App";
 import {AuthProvider} from './context/AuthContext'
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(
   <AuthProvider>
     <App />
